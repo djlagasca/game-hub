da6e305184cca882e8b0e86015875a2145694006
Denverjack Lagasca <djlagasca@gmail.com>
2025-12-11 13:08:12 +0800

    adjust for deployment on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/game-hub/",
+  base: "/",
   plugins: [react(), tsconfigPaths()],
 });
