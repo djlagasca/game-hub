23bc80a328836ff1288b4806cabd48e019a1c32a
Denverjack Lagasca <djlagasca@gmail.com>
2025-12-11 11:05:12 +0800

    update title and logo

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
-import { Provider } from "@/components/ui/provider"
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
+import { Provider } from "@/components/ui/provider";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import logoUrl from "@/assets/logo.webp";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <Provider>
-            <App />
-        </Provider>
-    </React.StrictMode>,
-)
+  <React.StrictMode>
+    <Provider>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+const faviconLink = document.querySelector<HTMLLinkElement>("link[rel='icon']");
+if (faviconLink) {
+  faviconLink.type = "image/webp";
+  faviconLink.href = logoUrl;
+}
